41f998f7182475dc507b3d357c2748cc77c5e491
cristian <contato@camargodev.com.br>
2023-07-20 10:53:05 -0400

    fix : adjust enviroments

--- env.ts
+++ env.ts
@@ -15,11 +15,9 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
-	APP_KEY: Env.schema.string(),
+	HOST: Env.schema.string.optional({ format: 'host' }),
+	PORT: Env.schema.number.optional(),
 	APP_NAME: Env.schema.string(),
-  	DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
 
 	GOOGLE_SHEETS_API_KEY:Env.schema.string(),
